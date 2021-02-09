7b299bb33e399c226dca3d8aba2366ecaa9f1281
Stefan Jaud <ga24yuk@mytum.de>
2021-02-09 22:22:13 +0100

    initial commit generating documentation

---
+++ .github/workflows/texlive_compile.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# This script is used for building LaTeX files using GitHub actions
+
+# see also https://unix.stackexchange.com/questions/31414/how-can-i-pass-a-command-line-argument-into-a-shell-script
+# and      https://unix.stackexchange.com/questions/32290/pass-command-line-arguments-to-bash-script
+
+# check input
+if [ $# -ne 1 ]; then
+    echo $0: usage: texlive_compile filename
+    exit 1
+fi
+
+# set variable
+name=$1
+
+pdflatex -shell-escape -synctex=1 -extra-mem-top=10000000 $name.tex
+bibtex $name
+makeglossaries $name
+makeindex $name
+pdflatex -shell-escape -synctex=1 -extra-mem-top=10000000 $name.tex
+pdflatex -shell-escape -synctex=1 -extra-mem-top=10000000 $name.tex
