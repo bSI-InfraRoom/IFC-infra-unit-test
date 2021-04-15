bcf061f9d8523f75315b6a8b6e98b1d0c9927d29
Stefan Jaud <ga24yuk@mytum.de>
2021-04-15 11:19:50 +0200

    different syntax

--- .github/workflows/texlive_install.sh
+++ .github/workflows/texlive_install.sh
@@ -6,7 +6,7 @@
 # A minimal current TL is installed adding only the packages that are
 # required
 
-tlmgr --uninstall
+tlmgr uninstall luatex
 
 # See if there is a cached version of TL available
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
